dc27a48b45ca5f355cb15c34bb3f2215a8bd179b
Andrei Shtanakov <pr0sto@protonmail.com>
2021-02-01 20:04:47 +0500

    ДОбавлены скрипты сборки ядра из исходников

--- manual_kernel_update/packer/scripts/stage-1-kernel-update-source.sh
+++ manual_kernel_update/packer/scripts/stage-1-kernel-update-source.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
 
-# Install elrepo
-yum install -y http://www.elrepo.org/elrepo-release-7.0-3.el7.elrepo.noarch.rpm
-# Install new kernel
-yum --enablerepo elrepo-kernel install kernel-ml -y
+
+# Установка более свежей версии gcc, поскольку в репозитории sentosсамая последняя версия - 4
+# Устанавливаем самым простым способом через пакет разработчика devtoolset-7
+yum install centos-release-scl -y
+yum install devtoolset-7 -y
+# устанавливаем переменную $PATH таким образом, что бы вызывался gcc 7 версии
+export PATH=/opt/rh/devtoolset-7/root/usr/bin:/opt/rh/devtoolset-7/:$PATH
+#scl enable devtoolset-7 bash
+# Устанавливаем дополнительные инструментыю wget можнор не ставить, можно использовать curl
+yum install -y ncurses-devel make bc bison flex elfutils-libelf-devel openssl-devel grub2
+yum install wget -y
+cd /usr/src
+# закачиваем исходники ядра версии 5.9.9
+wget https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.9.9.tar.xz
+tar -xvf linux-5.9.9.tar.xz
+cd linux-5.9.9
+# сборка и компиляция ядра
+cp /boot/config-* .config
+make olddefconfig
+make -j2
+make modules_install
+make install
 # Remove older kernels (Only for demo! Not Production!)
 rm -f /boot/*3.10*
 # Update GRUB
@@ -12,3 +30,4 @@ grub2-set-default 0
 echo "Grub update done."
 # Reboot VM
 shutdown -r now
+
