8f71c1e5b8bb324e5441f1e134e08b2964502c95
Andrei Shtanakov <pr0sto@protonmail.com>
2021-02-06 22:29:23 +0500

    Task 3

---
+++ RAID-types/RAID1/script.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Занулим суперблоки:
+mdadm --zero-superblock --force /dev/sd{b,c,d,e,f}
+# Удаляем старые метаданные и подпись на дисках:
+wipefs --all --force /dev/sd{b,c,d,e,f}
+# Создаем массив 10 уровня из 5 дисков
+mdadm --create --verbose /dev/md0 -l 10 -n 5 /dev/sd{b,c,d,e,f}
+mkdir /etc/mdadm
+# Создание конфигурационного файла mdadm.conf
+echo "DEVICE partitions" > /etc/mdadm/mdadm.conf
+mdadm --detail --scan --verbose | awk '/ARRAY/ {print}' >> /etc/mdadm/mdadm.conf
+# Создаем раздел GPT на RAID
+parted -s /dev/md0 mklabel gpt
+# Создаем партиции
+parted /dev/md0 mkpart primary ext4 0% 20%
+parted /dev/md0 mkpart primary ext4 20% 40%
+parted /dev/md0 mkpart primary ext4 40% 60%
+parted /dev/md0 mkpart primary ext4 60% 80%
+parted /dev/md0 mkpart primary ext4 80% 100%
+# Создаем ФС
+for i in $(seq 1 5); do sudo mkfs.ext4 /dev/md0p$i; done
+# Монтирование их по каталогам
+mkdir -p /raid/part{1,2,3,4,5}
+for i in $(seq 1 5); do mount /dev/md0p$i /raid/part$i; done
+
+echo "/dev/md0p1 /raid/part1    ext4    defaults    0 0" >> /etc/fstab
+echo "/dev/md0p2 /raid/part2    ext4    defaults    0 0" >> /etc/fstab
+echo "/dev/md0p3 /raid/part3    ext4    defaults    0 0" >> /etc/fstab
+echo "/dev/md0p4 /raid/part4    ext4    defaults    0 0" >> /etc/fstab
+echo "/dev/md0p5 /raid/part5    ext4    defaults    0 0" >> /etc/fstab
+
